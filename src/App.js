6ac559806442a79c73952b00c471d9b750d54d8f
olayinka <olayinkaolodude5@gmail.com>
2024-07-22 11:49:45 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ const initialState = {
   balance: 0,
   loan: 0,
   disabled: true,
+  errorMsg: ''
 };
 const reducer = (state, action) => {
   switch (action.type) {
@@ -13,21 +14,23 @@ const reducer = (state, action) => {
     case "LOAN":
       return {
         ...state,
-        loan: state.loan ===0? 5000 : state.loan,
-        balance: state.loan ===0?  state.balance + 5000 : state.balance,
+        loan: state.loan ===0? action.payload : state.loan,
+        balance: state.loan ===0?  state.balance + action.payload : state.balance,
       };
     case "DEPOSIT":
-      return { ...state, balance: state.balance + 150 };
+      return { ...state, balance: state.balance + action.payload };
     case "WITHDRAW":
-      return { ...state, balance: state.balance - 50 };
+      return { ...state, balance:state.balance>0 ? state.balance - action.payload : state.balance};
     case "PAY_LOAN":
       return { ...state, balance: state.balance - state.loan, loan: 0 };
 
       case "CLOSE_ACCOUNT":
-        return {
-          ...state,
-          disabled: state.balance === 0 && state.loan === 0 ? true : false,
-        };
+          return {
+              ...state,
+              disabled: state.balance === 0 && state.loan === 0,
+              errorMsg: state.balance > 0 || state.loan > 0 ? "Cannot close account with balance or loan." : false
+          };
+      
         
         
     default:
@@ -37,30 +40,31 @@ const reducer = (state, action) => {
 
 function App() {
   const [state, dispatch] = useReducer(reducer, initialState);
-  const { balance, disabled, loan } = state;
+  const { balance, disabled, loan, errorMsg} = state;
   return (
     <div className="App">
       <h1>UseReducer Bank Account 🏦</h1>
+      <i>{errorMsg}</i>
       <p>Balance:${balance}</p>
       <p>Loan:${loan}</p>
-      <button onClick={() => dispatch({ type: "OPEN_ACCOUNT" })}>
+      <button onClick={() => dispatch({ type: "OPEN_ACCOUNT" })} disabled={!disabled}>
         Open account
       </button>
-      <button disabled={disabled} onClick={() => dispatch({ type: "DEPOSIT" })}>
+      <button disabled={disabled} onClick={() => dispatch({ type: "DEPOSIT", payload:150 })}>
         Deposit 150
       </button>
       <button
         disabled={disabled}
-        onClick={() => dispatch({ type: "WITHDRAW" })}
+        onClick={() => dispatch({ type: "WITHDRAW", payload:50})}
       >
         Withdraw 50
       </button>
-      <button disabled={disabled} onClick={() => dispatch({ type: "LOAN" })}>
+      <button disabled={disabled} onClick={() => dispatch({ type: "LOAN", payload:5000 })}>
         Request a loan of 5000
       </button>
       <button
         disabled={disabled}
-        onClick={() => dispatch({ type: "PAY_LOAN" })}
+        onClick={() => dispatch({ type: "PAY_LOAN",})}
       >
         Pay a loan
       </button>
